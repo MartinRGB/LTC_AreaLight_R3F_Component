7ba2176d431f84faaab43564ce2820dada38917f
MartinRGB <qiuyinsen@gmail.com>
2023-07-03 17:56:38 +0800

    Update LTCAreaLight.tsx

--- src/LTCAreaLight/LTCAreaLight.tsx
+++ src/LTCAreaLight/LTCAreaLight.tsx
@@ -9,6 +9,46 @@ import { DOWNSAMPLE_BLUR, UPSAMPLE_BLUR } from "./shader/DualKawaseBlur_Shader";
 import { common_vertex_main, prefix_frag, prefix_vertex } from "./shader/Utils";
 import mergeRefs from 'react-merge-refs';
 
+
+// *** The Process of LTCAreaLight ***
+
+// *** What LTCAreaLight did ***
+// 1. According to the properties of the 'LTCAreaLight' object，add the following properties to the Ref object:
+//      - isDoubleSide
+//      - isClipless
+//      - rectAreaLightTexture (To improve performance, the texture is blurred in advance using DualKawaseBlur,and the FBO size can be set)
+// 2. In 'UseFrame',Continuous Blur Material with DualKawaseBlur
+// 3. Create a Plane Mesh to display the image or video texture as LightHelper.
+
+// *** What LTCAreaLightProxy did ***
+// 1. Init the LTC Texture  -> RectAreaLightUniformsLib.init() -> which will generate ltc1/ltc2 Texture for LTC_Evaluate
+// 2. Traverse all objects in the scene, and find the 'LTCAreaLight' object, and add the following properties to the Ref object(Array):
+//     - isDoubleSide
+//     - isClipless
+//     - rectAreaLightTexture 
+// 3. Traverse all objects in the scene, and find the 'Mesh' object,
+//     - add these properties(in step 2) as uniforms into them:
+//     - modify shaders in onBeforeCompile
+
+// *** What the shaders have changed ***
+// The three main shaders in Three.js have been modified
+//     -lights_pars_begin
+//     -lights_fragment_begin
+//     -lights_physical_pars_fragment
+
+// 1. In 'lights_pars_begin', add the following uniforms:
+//      uniform bool enableRectAreaLightTextures[ NUM_RECT_AREA_LIGHTS ];
+//      uniform bool isCliplesses[ NUM_RECT_AREA_LIGHTS ];
+//      uniform bool isDoubleSides[ NUM_RECT_AREA_LIGHTS ];
+
+// 2. In 'lights_fragment_begin', add these uniforms into the 'rectAreaLight' function:
+//      RE_Direct_RectArea( rectAreaLight, geometry, material, rectAreaLightTextures[ i ],enableRectAreaLightTextures[i],isDoubleSides[i],isCliplesses[i],reflectedLight );
+
+// 3. In 'lights_physical_pars_fragment',this part mainly based on SelfShadow's 'ltc_code'(https://github.com/selfshadow/ltc_code):
+//     - Modified the calculation of roughness in ‘RE_Direct_RectArea’ so that it is influenced by the map color
+//     - Modified ‘LTC_Evaluate's algorithm to include texture sampling 
+//     - 'FilteredBorderRegion' is used to blur & clamp the edges of the texture when it is scaled according to roughness in 'LTC_Evaluate'
+
 export const LTCAreaLightProxy = React.forwardRef(({ 
     children,
 }:{
