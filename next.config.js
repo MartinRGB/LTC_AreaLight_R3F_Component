e53a7802ffa02a3173c7b96c24f039cd340205a7
MartinRGB <qiuyinsen@gmail.com>
2023-07-03 21:10:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-  },
-  typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-  },
+  output: 'export'
 }
 module.exports = nextConfig
